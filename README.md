f0f41025dcae191e04eb5ffa251f827b5d3012da
Rober <rr92va@gmail.com>
2016-04-27 17:16:39 +0200

    Change base proyect. Add Mustache template for php.

--- README.md
+++ README.md
@@ -2,10 +2,8 @@
 ====================================READ THIS=====================================
 You need to init the php server with your steam key.
 
-Example:
+Example(Linux):
 
 $ API_KEY="YOUR_KEY_HERE" php -S localhost:8080 main.php
 
 ==================================================================================
-
-
--- utils.php
+++ utils.php
@@ -1,8 +1,33 @@
 <?php
-	
-	//Return the ID64 of user.
-	function getId($idUser){
-		return $id64;
-	}
+  function isGameSupported($idJuego){
+    switch ($idJuego) {
+      case '10':
+        return true;
+        break;
 
+      default:
+        //echo 'Juego no soportado';
+        break;
+    }
+  }
+  
+  function gameData($idJuego, $name){
+    switch ($idJuego) {
+      case '10':
+        game10($name);
+        break;
+
+      default:
+        //echo 'Juego no soportado';
+        break;
+    }
+  }
+
+  function game10($name){
+
+  }
+
+  function game20($name){
+    echo 'juego correcto';
+  }
 ?>
